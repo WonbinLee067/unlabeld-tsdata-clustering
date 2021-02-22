ddaf54167ed41ace93704024ffb97ad7fb72ab84
MyName <MyEmail>
2021-02-22 20:42:22 +0900

    Setting parameters for Time Series KMeans

--- UI/core_components.py
+++ UI/core_components.py
@@ -1,5 +1,5 @@
 import dash_core_components as dcc
-
+import dash_daq as daq
 def tab_layout(id, init_value, children):
     return dcc.Tabs(id=id, value=init_value,
         children = [dcc.Tab(label=lbl, value=val) for lbl, val in children.items()]
@@ -35,10 +35,19 @@ def txt_input_layout(id, init_value='', input_type='text', placeholder='input va
 def num_input_layout(id, min=0, max=50, init_value=1, placeholder=""):
     return dcc.Input(value=init_value, id=id, type='number', min=min, max=max)
 
-def slider_layout(id, min, max, marks,init_value=1):
+def slider_layout(id, min, max, marks,init_value=1, step=1):
     return dcc.Slider(
         min=min, 
         max=max, 
         marks=marks ,
         value=init_value, 
-        id=id)
+        id=id,
+        step=step)
+
+def switch_layout(id, on=False, label=""):
+    return daq.BooleanSwitch(
+        id = id,
+        on = on,
+        label = label,
+        labelPosition="top"
+    )
