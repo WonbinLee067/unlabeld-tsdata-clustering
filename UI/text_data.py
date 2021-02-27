604d55b9d00af332bd2e1720acb506f3e7096744
beom <rqt9207@gmail.com>
2021-02-27 23:38:04 +0900

    Update text_data
    변수 추가

--- UI/text_data.py
+++ UI/text_data.py
@@ -8,16 +8,17 @@ external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
-sample_num = "2";
+cluster_num = "7"
+data_num = "150"
+time = "30"
+used_algo = "DTW"
 
 app.layout = html.Div(children=[
-    html.H1(children='군집 개수 : '+sample_num),
-    html.H1(children='군집별 데이터 개수 : '+sample_num),
-    html.H1(children='총 소요 시간 : '+sample_num),
-    html.H1(children='사용된 알고리즘 : '+sample_num),
-    html.Div(children='''
-        Dash: A web application framework for Python.
-    '''),
+    html.H3(children='군집 개수 : '+cluster_num),
+    html.H3(children='군집별 데이터 개수 : '+data_num),
+    html.H3(children='총 소요 시간 : '+time),
+    html.H3(children='사용된 알고리즘 : '+used_algo),
+    
 ])
 
 if __name__ == '__main__':
