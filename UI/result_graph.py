b8f6b9be02f74e9746b7a9b88a4212c352e35193
Dahye Shim <shimshim485@naver.com>
2021-03-24 15:30:55 +0900

    Update result_graph.py

--- UI/result_graph.py
+++ UI/result_graph.py
@@ -10,20 +10,16 @@ import plotly.graph_objects as go
 # n: 한 군집 내 시계열 데이터 개수
 # label: 딕셔너리 keys(list형태)
 # color: 그래프 line 색
-def makeGraph(graph, n, label, color):
-    df=[]
-    for i in range(0, n):
-        df.append(graph[label[i]])
-
+def makeGraph(graph, color):
+    
     fig = go.Figure()
-    for i in range(0, n):
-        fig.add_trace(go.Scatter(y=df[i], name=label[i], line=dict(color=color)))
-
+    for i in range(0, len(graph)):
+        fig.add_trace(go.Scatter(y=graph[i],  line=dict(color=color)))
     return fig
     
 # figure: makeGraph()를 이용해 만든 그래프
 # label: 그래프 이름
-def updateLayout(figure, name, yaxis='value'):
+def updateLayout(figure,name,  yaxis='value'):
     figure.update_layout(
         title=name,
         yaxis_title=yaxis
@@ -33,36 +29,49 @@ def updateLayout(figure, name, yaxis='value'):
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 
-GG = {
-    'gr1':[0,1,2,5,4,8],
-    'gr2':[5,2,4,6,5,4,3,7],
-    'gr3':[4,11,2,6,5,1,2,7],
-    'gr4':[7,4,5,7,4,1]
-}
+GG = [
+    [[0,1,2,5,4,8],
+    [5,2,4,6,5,4,3,7],
+    [4,11,2,6,5,1,2,7],
+    [7,4,5,7,4,1]],
+    [[4,5,3,6,2],
+    [4,4,9,8,0,9],
+    [2,4,3,5,3,3,3,8],
+    [5,4,6,5,5,5]],
+    [[4,2,3,2,4,3,4],
+    [7,6,5,6,6,5,4],
+    [3,2,3,4,5,4]]
+]
 
 colors = {
     'background': 'dimgray',
     'text': 'white'
 }
-
-fig = makeGraph(GG, 4, list(GG.keys()), 'teal')
-updateLayout(fig, 'GG1')
+fig=[]
+for i in range(0,len(GG)):
+    fig.append(makeGraph(GG[i], 'teal'))
+    updateLayout(fig[i], 'cluster'+str(i))
 
 def graphDiv():
-    graph = html.Div(style={'backgroundColor': colors['background']}, children=[
-        html.H1(
-            children='AXISX',
-            style={
-                'textAlign': 'center',
-                'color': colors['text']
-            }
+    graph = html.Div(style={'backgroundColor': colors['background'], 'width':"30%", 'height':'100px'}, children=[
+
+        dcc.Graph(
+            id='G1',
+            figure=fig[0]
         ),
 
         dcc.Graph(
-            id='GG',
-            figure=fig
-        )
+            id='G2',
+            figure=fig[1]
+        ),
+
+        dcc.Graph(
+            id='G3',
+            figure=fig[2]
+        ),
     ])
+    
+
     return graph
 
 
