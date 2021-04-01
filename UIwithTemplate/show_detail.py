ed149dcd55cf2eb6dd8016bba789980cad8fd8b1
MoSangIl <mosang1230@naver.com>
2021-04-01 19:46:22 +0900

    Edit UI for choosing What cluster is shown

--- UIwithTemplate/show_detail.py
+++ UIwithTemplate/show_detail.py
@@ -1,9 +1,15 @@
 import dash_html_components as html
 import core_components as cc
-
+from result_graph import num_clusters
 def detailGraphOption():
     return [
-        html.Label("Choose Type of detailed graphs"),
+        html.Label("Choose cluster"),
+        cc.dropdown_layout("nth-cluster",
+        [
+            {'label': str(i+1), 'value': i}
+            for i in range(num_clusters)
+        ], 0),
+        html.Label("Choose Type"),
         cc.dropdown_layout("detail-graph-input", 
         [
             {'label': 'Graph Detail', 'value': 'GrDt'},
